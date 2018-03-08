19efb74e5f297a685366eb1164e901853bd0ea81
Dylan <21029693+Dy1ski@users.noreply.github.com>
2018-03-08 18:15:29 +1100

    Update AILab2.java

--- AILab2.java
+++ AILab2.java
@@ -7,6 +7,7 @@ package ai.lab2;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 public class AILab2 {
@@ -18,12 +19,15 @@ public class AILab2 {
      public static void main(String[] args) throws Exception 
     {
         String[] arr;
+        // List<String> afc = Arrays.asList(arr);
+        List<String> afc = new ArrayList<>();
+        List<String> lTotal = new ArrayList<>();
         FileReader file = new FileReader("D:/travel.txt");
         BufferedReader reader = new BufferedReader(file);
         
        
        // String text = "";
-        //String line = reader.readLine();
+       // String line = reader.readLine();
         
         
         for(String line = reader.readLine(); line != null; line = reader.readLine())
@@ -31,25 +35,26 @@ public class AILab2 {
             arr = line.split("\\s+");
             //System.out.println(line);
             //System.out.println(Arrays.toString(arr));
+             
+           // for loop here with combiing the list
             
-            List<String> afc = Arrays.asList(arr);
-            System.out.println(afc);
-            System.out.println("next line");
-            
+            afc = Arrays.asList(arr);
+            if (afc.get(2).contains("-1"))
+            {
+                System.out.println("Cant drive from " + afc.get(0) + " to city " + afc.get(1) + " there is a straight line distance of " + afc.get(3));
+            }
+            else
+            {
+                System.out.println("Travel from city " + afc.get(0) + " to city " + afc.get(1) + " with a stright line distance of " + afc.get(2) + " and an actual distance of " + afc.get(3));
+            }
             
         }
-//        while(line != null){
-//        text += line + "\n";
-//        line = reader.readLine();
-//        String[] arr = text.split(" ");
         
-        //System.out.println(Arrays.toString(arr));
+        reader.close();
         
     }
         
-        //reader.close();
-        //System.out.println(text);
+        
+       
         
 }
-    
-//}
